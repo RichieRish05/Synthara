773ed5aef8d051fc32b004b035b37405ab226515
Rishi Murumkar <108751289+RichieRish05@users.noreply.github.com>
2025-09-11 00:36:55 -0700

    auth config tests

--- frontend/src/env.js
+++ frontend/src/env.js
@@ -20,7 +20,8 @@ export const env = createEnv({
     GENERATE_FROM_DESCRIPTION: z.string(),
     GENERATE_FROM_DESCRIBED_LYRICS: z.string(),
     GENERATE_WITH_LYRICS: z.string(),
-    BETTER_AUTH_SECRET: z.string()
+    BETTER_AUTH_SECRET: z.string(),
+    BETTER_AUTH_URL: z.string()
   },
 
   /**
@@ -49,6 +50,7 @@ export const env = createEnv({
     GENERATE_FROM_DESCRIBED_LYRICS: process.env.GENERATE_FROM_DESCRIBED_LYRICS,
     GENERATE_WITH_LYRICS: process.env.GENERATE_WITH_LYRICS,
     BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
+    BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
     NEXT_PUBLIC_BETTER_AUTH_URL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
